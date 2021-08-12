e51b0820689109ec614ee704f3c6ae3b41ea34a4
monaSalih <monaalobisat@gmail.com>
2021-08-12 15:05:43 +0300

    work

--- src/component/SelectList.js
+++ src/component/SelectList.js
@@ -2,19 +2,25 @@ import React from 'react';
 import Form from 'react-bootstrap/Form';
 
 class SelectList extends React.Component{
-    renderImage=(event)=>{
+    // renderImage=(event)=>{
     
-    ///////////////test
+    // ///////////////test
+    // }
+    numberOfHonerd=(event)=>{this.props.numberOfHonerd(
+        event.target.value)
     }
+    
+
     render(){
         return(
+            // onClick={this.props.numberOfHonerd}
             <>
             <Form>
-            <Form.Select onChange={this.props.numberOfHonerd} >
+            <Form.Select onChange={this.numberOfHonerd}  >
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
-            <option value="4">4</option>
+            <option value="100">100</option>
         </Form.Select>
         </Form>
         </>
